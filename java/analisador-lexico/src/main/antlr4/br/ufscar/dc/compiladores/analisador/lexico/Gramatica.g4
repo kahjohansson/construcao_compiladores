37c2c86cc2a5eb2c9c94a74311d12e781da0fc7d
Karina <kahjohansson@gmail.com>
2021-03-06 11:59:34 -0300

    alter and refatorate code to pass in the first test case

--- java/analisador-lexico/src/main/antlr4/br/ufscar/dc/compiladores/analisador/lexico/Gramatica.g4
+++ java/analisador-lexico/src/main/antlr4/br/ufscar/dc/compiladores/analisador/lexico/Gramatica.g4
@@ -1,15 +1,20 @@
 lexer grammar Gramatica;
 
-
-ALGORITMO: 'algoritmo';
-DECLARE: 'declare';
-FIM_ALGORITMO: 'fim_algoritmo';
-LEIA: 'leia';
-ESCREVA: 'escreva';
+PALAVRA_CHAVE: 'algoritmo' | 'declare' | 'fim_algoritmo' | 'leia' | 'escreva' | 'inteiro' | 'literal';
 
 IDENT: LETRA(LETRA|NUM_INT)*;
+CADEIA: '"' ~('\n' | '\t' | '\r' | '"')* '"';
+
 NUM_INT: ('+'|'-')? ('0'..'9');
 
+
+SIMBOLO: '(' | ')' | ',' | ':';
+OPERADOR_MAT: '+';
+OPERADOR_LOG: 'e' | 'ou' | 'nao' | '<>' | '=';
+OPERADOR_OUTROS: '<-' | '^';
+
+COMENTARIO: '{' ~('\n')* '}' -> skip;
+ESPACO: (' ' | '\t' | '\r' | '\n' ) -> skip;
 
 fragment
 LETRA: 'a'..'z' | 'A'..'Z';
--- java/analisador-lexico/src/main/java/br/ufscar/dc/compiladores/analisador/lexico/Main.java
+++ java/analisador-lexico/src/main/java/br/ufscar/dc/compiladores/analisador/lexico/Main.java
@@ -1,6 +1,8 @@
 package br.ufscar.dc.compiladores.analisador.lexico;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
@@ -10,10 +12,18 @@ public class Main {
         try {
             CharStream cs = CharStreams.fromFileName(args[0]);
             Gramatica gram = new Gramatica(cs);
+            ArrayList<String> labels = new ArrayList<String>(Arrays.asList("PALAVRA_CHAVE", "SIMBOLO"));
 
             Token t = null;
             while ((t = gram.nextToken()).getType() != Token.EOF) {
-                System.out.println("<"+ Gramatica.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText()+">");
+                String tipo = Gramatica.VOCABULARY.getDisplayName(t.getType());
+                String valor = t.getText();
+                
+                if (labels.contains(tipo)){
+                    System.out.println("<'"+ valor + "','" + valor +"'>");
+                } else{
+                    System.out.println("<'"+ valor + "'," + tipo +">");
+                }
             }
         } catch (IOException ex) {
         }
