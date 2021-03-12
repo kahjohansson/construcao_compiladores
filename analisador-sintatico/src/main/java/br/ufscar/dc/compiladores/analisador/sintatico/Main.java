d751f98719948aa822724bae67a4018a09fbeb3f
Karina <kahjohansson@gmail.com>
2021-03-12 11:17:33 -0300

    add comments to code

--- analisador-sintatico/src/main/java/br/ufscar/dc/compiladores/analisador/sintatico/Main.java
+++ analisador-sintatico/src/main/java/br/ufscar/dc/compiladores/analisador/sintatico/Main.java
@@ -12,14 +12,14 @@ import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.RecognitionException;
 
 /*
-Classe principal e única do analisador léxico
-input: arquivo a ser analisado léxicamente segundo a gramática LA
-output: arquivo com o resultado da análise léxica, cada linha possui um token
+Classe principal e única do analisador léxico e sintático
+input: arquivo a ser analisado léxicamente e sintáticamente segundo a gramática LA
+output: arquivo com erros léxicos e sintáticos, se houverem
 */
 public class Main {
     public static void main(String args[]) {
                 
-        try(PrintWriter pw = new PrintWriter(new File(args[1]))) {
+        try(PrintWriter pw = new PrintWriter(new File(args[1]))) { // instância do escritor do arquivo de log
             
             CharStream cs = CharStreams.fromFileName(args[0]); //manipula arquivo de entrada
             GramaticaLexer lexer = new GramaticaLexer(cs);
@@ -28,7 +28,7 @@ public class Main {
                     "PALAVRA_CHAVE", "SIMBOLO", "OPERADOR_LOG_PALAVRA", 
                     "OPERADOR_MAT", "OPERADOR_LOG", "OPERADOR_OUTROS")); //listagem dos padrões com token do formato <lexema,lexema>
 
-            boolean error = false;
+            boolean error = false; // variável que indica se houve erro léxico
             
             Token t = null; //instancia variável do token
             while ((t = lexer.nextToken()).getType() != Token.EOF) { //laço que itera os tokens até o fim do arquivo
@@ -53,22 +53,22 @@ public class Main {
                 }
             }
      
-            if (! error){
+            if (! error){ // se não houve erro léxico, é feita a análise sintática
             
                 CharStream cs1 = CharStreams.fromFileName(args[0]);
                 GramaticaLexer lexer1 = new GramaticaLexer(cs1);
                 CommonTokenStream tokens = new CommonTokenStream(lexer1);
-                GramaticaParser parser = new GramaticaParser(tokens);
+                GramaticaParser parser = new GramaticaParser(tokens); // instância do parser
 
-                MyCustomErrorListener mcel = new MyCustomErrorListener(pw);
+                MyCustomErrorListener mcel = new MyCustomErrorListener(pw); // instância do tratador de erros
                 parser.addErrorListener(mcel);
 
                 try{
-                    parser.programa();
+                    parser.programa(); // tenta fazer a análise do programa
                 } catch (Exception e){
-                    pw.println(e.getMessage());
+                    pw.println(e.getMessage()); // recupera a mensagem de erro causada pela exceção do tratador de erros
                 }
-                pw.println("Fim da compilacao");
+                pw.println("Fim da compilacao"); // mensagem final do arquivo de log de análise léxica e sintática
             }
             
         } catch (Exception e){
--- analisador-sintatico/src/main/java/br/ufscar/dc/compiladores/analisador/sintatico/MyCustomErrorListener.java
+++ analisador-sintatico/src/main/java/br/ufscar/dc/compiladores/analisador/sintatico/MyCustomErrorListener.java
@@ -2,17 +2,19 @@ package br.ufscar.dc.compiladores.analisador.sintatico;
 
 import java.io.PrintWriter;
 import java.util.BitSet;
-import org.antlr.v4.runtime.ANTLRErrorListener; // cuidado para importar a versão 4
+import org.antlr.v4.runtime.ANTLRErrorListener;
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
-import org.antlr.v4.runtime.Token; // Vamos também precisar de Token
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.atn.ATNConfigSet;
 import org.antlr.v4.runtime.dfa.DFA;
-// Outros imports vão ser necessários aqui. O NetBeans ou IntelliJ fazem isso automaticamente
 
+
+/* classe de tratamento de erros sintáticos */
 public class MyCustomErrorListener implements ANTLRErrorListener {
     
+    // printer para preencher log com erros sintáticos
     PrintWriter pw;
     public MyCustomErrorListener(PrintWriter pw){
         this.pw = pw;
@@ -20,31 +22,31 @@ public class MyCustomErrorListener implements ANTLRErrorListener {
     
     @Override
     public void	reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
-        // Não será necessário para o T2, pode deixar vazio
+        
     }
     
     @Override
     public void reportAttemptingFullContext(Parser recognizer, DFA dfa, int startIndex, int stopIndex, BitSet conflictingAlts, ATNConfigSet configs) {
-        // Não será necessário para o T2, pode deixar vazio
+        
     }
 
     @Override
     public void reportContextSensitivity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, int prediction, ATNConfigSet configs) {
-        // Não será necessário para o T2, pode deixar vazio
+       
     }
 
+    /* método para tratar os erros sintáticos */
     @Override
     public void	syntaxError(Recognizer<?,?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-        // Aqui vamos colocar o tratamento de erro customizado
-
         
         Token t = (Token) offendingSymbol;
         String token = t.getText();
         
+        // caso especial para erro de EOF para adequar à saída esperada
         if (token.equals("<EOF>")){
             token = "EOF";
             pw.println("Linha "+ line +": erro sintatico proximo a "+token);
-        }else{
+        }else{ // exceção gerada para erros sintáticos exceto para erro de EOF
             throw new RuntimeException("Linha "+line+": erro sintatico proximo a "+token);
         }
     }
