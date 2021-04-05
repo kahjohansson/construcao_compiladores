43b90733b201f7dc76191cc2fe22dfd61517ac2b
Karina Johansson <kahjohansson@gmail.com>
2021-04-05 11:30:25 -0300

    add auxiliar functions to the semantic analyser

--- analisador-semantico/src/main/java/br/ufscar/dc/compiladores/analisador/semantico/AnalisadorSemanticoLib.java
+++ analisador-semantico/src/main/java/br/ufscar/dc/compiladores/analisador/semantico/AnalisadorSemanticoLib.java
@@ -6,35 +6,54 @@ import java.util.List;
 import org.antlr.v4.runtime.Token;
 
 public class AnalisadorSemanticoLib {
-    
+
     public static List<String> errosSemanticos = new ArrayList<>();
-    
+
     public static void adicionarErroSemantico(String mensagem) {
         errosSemanticos.add(mensagem);
     }
-    
+
     public static TabelaSimbolos.TipoLA verificarTipo(TabelaSimbolos tabela, String nomeVar) {
         return tabela.verificar(nomeVar);
     }
-        
-    public static String getNomeRegistro(String nomeVariavelCompleto){
-        if(nomeVariavelCompleto.contains("."))
+
+    public static String getNomeRegistro(String nomeVariavelCompleto) {
+        if (nomeVariavelCompleto.contains(".")) {
             return nomeVariavelCompleto.substring(0, nomeVariavelCompleto.indexOf("."));
+        }
         return null;
     }
-    
-    public static String getNomeVariavel(String nomeVariavelCompleto){
-        if(nomeVariavelCompleto.contains("."))
-            return nomeVariavelCompleto.substring(nomeVariavelCompleto.indexOf(".")+1);
+
+    public static String getNomeVariavel(String nomeVariavelCompleto) {
+        if (nomeVariavelCompleto.contains(".")) {
+            return nomeVariavelCompleto.substring(nomeVariavelCompleto.indexOf(".") + 1);
+        }
         return nomeVariavelCompleto;
     }
-    
-        // retorna tipo TipoLA dada uma string
+
+    public static String getNomeRegistro(GramaticaParser.IdentificadorContext ctx) {
+        if(ctx.identLista.size() > 0){
+            return ctx.ident1.getText();
+        }
+        
+        return null;
+    }
+
+    public static String getNomeVariavel(GramaticaParser.IdentificadorContext ctx) {
+        
+        if(ctx.identLista.size() > 0){
+            return ctx.identLista.get(0).getText();
+        }
+        
+        return ctx.ident1.getText();
+    }
+
+    // retorna tipo TipoLA dada uma string
     public TipoLA getTipoLa(String tipo) {
-        
+
         TipoLA tipoLa = TipoLA.INVALIDO;
-        
-        switch (tipo){
+
+        switch (tipo) {
             case "literal":
                 tipoLa = TipoLA.LITERAL;
                 break;
@@ -50,7 +69,7 @@ public class AnalisadorSemanticoLib {
             default:
                 break;
         }
-        
+
         return tipoLa;
     }
 }
